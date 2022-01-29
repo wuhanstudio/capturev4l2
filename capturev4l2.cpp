2ce5de98f0b0543340fcdc3ab6fa2d259bb9fa43
wuhanstudio <wuhanstudio@qq.com>
2022-01-29 15:14:37 +0000

    Add YV12 Support

--- capturev4l2.cpp
+++ capturev4l2.cpp
@@ -82,6 +82,49 @@ uint64_t compressYUYVtoJPEG(const uint8_t* input, const int width, const int hei
     return outlen;
 }
 
+uint64_t compressYV12toJPEG(const uint8_t* input, const int width, const int height, uint8_t* &outbuffer) {
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    JSAMPROW row_ptr[1];
+    int row_stride;
+
+    // uint8_t* outbuffer = NULL;
+    uint64_t outlen = 0;
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+    jpeg_mem_dest(&cinfo, &outbuffer, &outlen);
+
+    // jrow is a libjpeg row of samples array of 1 row pointer
+    cinfo.image_width = width & -1;
+    cinfo.image_height = height & -1;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_YCbCr; //libJPEG expects YUV 3bytes, 24bit, YUYV --> YUV YUV YUV
+
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, 92, TRUE);
+    jpeg_start_compress(&cinfo, TRUE);
+
+    std::vector<uint8_t> tmprowbuf(width * 3);
+
+    JSAMPROW row_pointer[1];
+    row_pointer[0] = &tmprowbuf[0];
+    while (cinfo.next_scanline < cinfo.image_height) {
+        unsigned i, j;
+        for (i = 0, j = 0; i < cinfo.image_width; i += 1, j += 3) { //input strides by 4 bytes, output strides by 6 (2 pixels)
+            tmprowbuf[j + 0] = input[i + cinfo.image_width * cinfo.next_scanline]; // Y (unique to this pixel)
+            tmprowbuf[j + 1] = input[i / 2 + cinfo.image_width * cinfo.image_height + (cinfo.image_width / 2) * (cinfo.next_scanline / 2)]; // U (shared between pixels)
+            tmprowbuf[j + 2] = input[i / 2 + cinfo.image_width * cinfo.image_height + (cinfo.image_width / 2) * (cinfo.image_height / 2) + (cinfo.image_width / 2) * (cinfo.next_scanline / 2)]; // V (shared between pixels)
+        }
+        jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
+
+    jpeg_finish_compress(&cinfo);
+    jpeg_destroy_compress(&cinfo);
+
+    return outlen;
+}
+
 static int xioctl(int fd, int request, void *arg)
 {
     int r;
@@ -272,18 +315,16 @@ int capture_image(int fd)
     imshow("view", out_img);
     // imwrite("output.jpg", out_img);
 
+    // You may do image processing here
 
-    // RGB to YUV
-    // cv::cvtColor(out_img, img, cv::COLOR_RGB2YUV);
+    // RGB to YV12
+    cv::cvtColor(out_img, img, cv::COLOR_RGB2YUV_YV12);
 
-    // YUV to YUYV
-
-
-    // YUYV to JPEG
+    // YV12 to JPEG
     uint8_t* outbuffer = NULL;
     cv::Mat input = img.reshape(1, img.total()*img.channels());
     std::vector<uint8_t> vec = img.isContinuous()? input : input.clone();
-    uint64_t outlen = compressYUYVtoJPEG(vec.data(), IMAGE_WIDTH, IMAGE_HEIGHT, outbuffer);
+    uint64_t outlen = compressYV12toJPEG(vec.data(), IMAGE_WIDTH, IMAGE_HEIGHT, outbuffer);
 
     printf("libjpeg produced %ld bytes\n", outlen);
 
